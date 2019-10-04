a354902af9202b883b743a9491fd832ed5653c60
alizza-ops <52742283+alizza-ops@users.noreply.github.com>
2019-10-04 23:08:49 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,11 @@
+function pembagian (){
+		var x=37;
+		var y=7;
+
+		return x/y;
+}
+
+console.log("nilai awal: " + pembagian());
+console.log("nilai round: " + Math.round(pembagian()));
+console.log("nilai floor: " + Math.floor(pembagian()));
+console.log("nilai ceil: " + Math.ceil(pembagian()));
